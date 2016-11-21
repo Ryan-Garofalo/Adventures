062f77e6749ec5542e8158baa0d736bbab7db64c
Ryan Garofalo <ryangarofalo100@gmail.com>
2016-11-21 14:31:48 -0700

    cleaned up a lot of stuff.  Made it look pretty, and added content

--- script.js
+++ script.js
@@ -40,7 +40,9 @@ app.controller("mainController", function($scope){
     author: 'Ryan Garofalo',
     commentsShow : false,
     commentForm : false,
-    description:"Bike trip",
+    description:"Mounatin biking in Moad was one of the greatest adventures of my life.  We were camping with a bunch of people for a group birthday.\
+    For biking we rode the Full Enchilada trail and Slick Rock.  Riding in Slick Rock was dream come true even though it wasn't the best riding.  The Full \
+    Enchilada trail however was one of the best I've ever done, 33 miles of mostly downhill!",
     comments:[
       {author:"Matt",
       comment:"I miss you Garf"},
@@ -61,7 +63,8 @@ app.controller("mainController", function($scope){
     author: 'Ryan Garofalo',
     commentsShow : false,
     commentForm : false,
-    description:"Skiing",
+    description:"Keystone is my favorite mountain to ski!  It has a wide variety of terrain including the best terrain park, backcountry, and trees.\
+      I know the mountain very well and have skied it many times!  It's also one of the closer mountains to Denver so I choose it more often then not!",
     comments:[
       {author:"Chelsea",
       comment:"I wish I didn't flake looks like a fun trip"},
@@ -69,6 +72,8 @@ app.controller("mainController", function($scope){
       comment:"Colorado looks awesome"},
       {author:"Eli",
       comment:"You guys never invite me"},
+      {author:"Rick",
+      comment:"You are so cool"},
     ],
     image : "https://scontent-sjc2-1.xx.fbcdn.net/t31.0-8/12646660_1551339295177259_2501688924199248160_o.jpg",
     date:moment().subtract(3, 'days').subtract(2, 'hours').calendar(),
@@ -81,14 +86,13 @@ app.controller("mainController", function($scope){
     author: 'Ryan Garofalo',
     commentsShow : false,
     commentForm : false,
-    description:"Camping Trip",
+    description:"The National Sand Dune was one of my favorite camping adventures of the year.  I went with my good friends Mike and Rick and we had a blast!\
+    The terrain is absolutely unbelievable and makes you feel like you're on another planet.  We did sandboarding, hiked Zapata Falls, and told great campfire tales.",
     comments:[
       {author:"Rick",
       comment:"Looks Awesome!!!"},
       {author:"Mike",
       comment:"I want to go there!"},
-      {author:"Eli",
-      comment:"Why didn't you invite me?"},
     ],
     image: 'https://scontent-sjc2-1.xx.fbcdn.net/t31.0-8/14196090_1648560528788468_4498452573718665730_o.jpg',
     date: moment().subtract(5, 'days').subtract(20, 'hours').calendar(),
